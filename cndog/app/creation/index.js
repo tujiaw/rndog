bc530b6977b7261f4571c3240cd5d7bcc5e4a4a7
tujiaw <tujiaw@tujiawdeMacBook-Pro.local>
2017-02-12 12:02:42 +0800

    render footer

--- cndog/app/creation/index.js
+++ cndog/app/creation/index.js
@@ -9,36 +9,85 @@ import {
   Image,
   TouchableHighlight,
   Dimensions,
+  ActivityIndicator,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import Config from '../common/config';
 
+let cacheResults = {
+  nextPage: 1,
+  total: 0,
+  items: [],
+};
+
 const width = Dimensions.get('window').width;
+
 export default class List extends Component {
   constructor(props) {
     super(props);
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows([])
+      dataSource: ds.cloneWithRows([]),
+      isLoading: false,
     };
+
+    this._fetchData = this._fetchData.bind(this);
+    this._hasMore = this._hasMore.bind(this);
+    this._fetchMoreData = this._fetchMoreData.bind(this);
+    this._renderRow = this._renderRow.bind(this);
+    this._renderFooter = this._renderFooter.bind(this);
   }
 
   componentDidMount () {
-    this._fetchData();
+    this._fetchData(1);
   }
 
-  _fetchData() {
-    fetch(Config.api.creation)
-    .then((response) => response.json())
-    .then((responseText) => {
-      if (responseText.success) {
-        this.setState({dataSource: this.state.dataSource.cloneWithRows(responseText.data)})
-      }
+  _fetchData(page) {
+    this.setState({
+      isLoading: true
     })
-    .catch((error) => {
-      console.warn(error);
-    })
+
+    let that = this
+    const params = `?page=${page}`
+    fetch(Config.api.creation + params)
+      .then((response) => response.json())
+      .then((responseText) => {
+        if (responseText.success) {
+          console.log(responseText.data);
+          setTimeout(() => {
+            var items = cacheResults.items.slice()
+            items = items.concat(responseText.data);
+            cacheResults.items = items;
+            cacheResults.total = responseText.total;
+            that.setState({
+              dataSource: this.state.dataSource.cloneWithRows(cacheResults.items),
+              isLoading: false,
+            })
+          }, 2000);
+          // this.setState({
+          //   dataSource: this.state.dataSource.cloneWithRows(cacheResults.items),
+          //   isLoading: false,
+          // })
+        }
+      })
+      .catch((error) => {
+        this.setState({
+          isLoading: false
+        })
+        console.warn(error)
+      })
+  }
+
+  _hasMore() {
+    return cacheResults.items.length < cacheResults.total;
+  }
+
+  _fetchMoreData() {
+    if (!this._hasMore() || this.state.isLoading) {
+      return
+    }
+    this._fetchData(cacheResults.nextPage)
   }
 
   _renderRow(rowData) {
@@ -66,6 +115,22 @@ export default class List extends Component {
     )
   }
 
+  _renderFooter() {
+    if (this._hasMore()) {
+      return (
+        <ActivityIndicator
+          style={styles.loadingMore}
+        />
+      )
+    } else if (cacheResults.items.length > 0) {
+      return (
+        <View style={styles.loadingMore}>
+          <Text style={styles.loadingText}>不要再扯了，已经没有了</Text>
+        </View>
+      )
+    }
+  }
+
   render() { 
     return (
       <View style={styles.container}>
@@ -75,8 +140,11 @@ export default class List extends Component {
         <ListView
           dataSource={this.state.dataSource}
           renderRow={this._renderRow}
+          renderFooter={this._renderFooter}
           automaticallyAdjustContentInsets={false}
           enableEmptySections={true}
+          onEndReached={this._fetchMoreData}
+          onEndReachedThreshold={20}
         />
       </View>
     )
@@ -151,7 +219,14 @@ var styles = StyleSheet.create({
   handleText: {
     paddingLeft: 12,
     fontSize: 18,
-    color: '#333'
+    color: '#333',
+  },
+  loadingMore: {
+    marginBottom: 60,
+  },
+  loadingText: {
+    color: '#777',
+    textAlign: 'center',
   }
 });
 
